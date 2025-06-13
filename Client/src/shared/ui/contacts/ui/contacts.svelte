20142b9ed9ea6c71c4380f012d1b2ed66132f267
Edanriell <Edanriell@gmail.com>
2025-06-13 13:00:50 +0300

    feature(contacts): created contacts component

---
+++ Client/src/shared/ui/contacts/ui/contacts.svelte
@@ -0,0 +1,25 @@
+<address class="relative mt-[25rem] desktop:mt-[32rem]">
+	<dl class="relative flex flex-col gap-y-[24rem] desktop:gap-y-[16rem]">
+		<div class="flex flex-row gap-x-[16rem] items-start">
+			<dt class="shrink-0 grow-0">
+				<span class="visually-hidden">Location</span>
+				<img alt="Location" height="24" src="images/vector/icons/location.svg" width="24">
+			</dt>
+			<dd class="font-[var(--font-family)] font-normal text-[14rem] leading-[171%] text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</dd>
+		</div>
+		<div class="flex flex-row gap-x-[16rem] items-start">
+			<dt class="shrink-0 grow-0">
+				<span class="visually-hidden">Phone number</span>
+				<img alt="Phone number" height="24" src="images/vector/icons/phone.svg" width="24">
+			</dt>
+			<dd class="font-[var(--font-family)] font-normal text-[14rem] leading-[171%] text-white"><a href="tel:15431234567">+1-543-123-4567</a></dd>
+		</div>
+		<div class="flex flex-row gap-x-[16rem] items-start">
+			<dt class="shrink-0 grow-0">
+				<span class="visually-hidden">Email</span>
+				<img alt="Email" height="24" src="images/vector/icons/email.svg" width="24">
+			</dt>
+			<dd class="font-[var(--font-family)] font-normal text-[14rem] leading-[171%] text-white"><a href="mailto:example@fylo.com">example@fylo.com</a></dd>
+		</div>
+	</dl>
+</address>
