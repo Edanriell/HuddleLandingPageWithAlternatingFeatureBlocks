8856605103d6424b2e92ec6d0fc860c53c1d7dd7
Edanriell <Edanriell@gmail.com>
2025-06-13 21:42:36 +0300

    feature(secondary-navigation): created secondary navigation

---
+++ Client/src/shared/ui/secondary-navigation/ui/secondary-navigation.svelte
@@ -0,0 +1,22 @@
+<nav class="relative mt-[41rem]">
+	<ul class="grid grid-rows-[auto] grid-cols-1 gap-y-[16rem] desktop:grid-flow-row desktop:grid-cols-2 desktop:gap-y-[12rem] desktop:gap-x-[143rem]">
+		<li class="relative desktop:order-1">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">About Us</a>
+		</li>
+		<li class="relative desktop:order-3">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">What We Do</a>
+		</li>
+		<li class="relative desktop:order-5">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">FAQ</a>
+		</li>
+		<li class="relative desktop:order-2">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Career</a>
+		</li>
+		<li class="relative desktop:order-4">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Blog</a>
+		</li>
+		<li class="relative desktop:order-6">
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Contact Us</a>
+		</li>
+	</ul>
+</nav>
