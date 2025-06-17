cf3e0c2da585787ae51b71a20047b4486fdb6807
Edanriell <Edanriell@gmail.com>
2025-06-17 19:43:04 +0300

    feature(header-hero-bg): created bg

--- Client/src/widgets/header/ui/header.svelte
+++ Client/src/widgets/header/ui/header.svelte
@@ -3,7 +3,9 @@
 	import {Link} from "@shared/ui/link/ui";
 </script>
 
-<header class="bg-red-400 flex flex-row items-center justify-between pr-[16rem] pl-[16rem] pt-[27rem] pb-[27rem] desktop:pr-[80rem] desktop:pl-[80rem] desktop:pt-[48rem] desktop:pb-[48rem]">
-	<Logotype colorScheme="Dark" />
-	<Link colorScheme="White" href="/">Try It Free</Link>
+<header class="bg-[#edfbff]">
+	<div class="relative z-2 flex flex-row items-center justify-between pr-[16rem] pl-[16rem] pt-[27rem] pb-[27rem] desktop:pr-[80rem] desktop:pl-[80rem] desktop:pt-[48rem] desktop:pb-[48rem]">
+		<Logotype colorScheme="Dark" />
+		<Link colorScheme="White" href="/">Try It Free</Link>
+	</div>
 </header>
--- Client/src/widgets/hero/ui/hero.svelte
+++ Client/src/widgets/hero/ui/hero.svelte
@@ -2,15 +2,105 @@
 	import {Link} from "@shared/ui/link/ui";
 </script>
 
-<section class="bg-red-400 pl-[35rem] pr-[35rem] pb-[72rem] desktop:flex desktop:flex-row desktop:pl-[80rem] desktop:pr-[80rem] desktop:pb-[152.33rem] desktop:gap-x-[54rem] desktop:items-center desktop:justify-between desktop:pt-[23rem]">
-	<div class="desktop:flex desktop:flex-col desktop:basis-[536rem] desktop:shrink-0 desktop:mt-[74rem] desktop:grow-1">
+<section class="bg-[#edfbff] pl-[35rem] pr-[35rem] pb-[72rem] desktop:flex desktop:flex-row desktop:pl-[80rem] desktop:pr-[80rem] desktop:pb-[152.33rem] desktop:gap-x-[54rem] desktop:items-center desktop:justify-between desktop:pt-[23rem]">
+	<svg class="hidden! absolute top-0 left-0 desktop:block!" fill="none" height="408" viewBox="0 0 437 408" width="437" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M0 408C0 408 58.4276 269.065 170.692 308.008C282.957 346.951 322.832 399.614 383.281 308.008C398.954 284.257 393.147 233.497 354.003 231.482C314.859 229.466 344.752 185.166 412.147 176.475C479.542 167.784 394.051 106.413 342.752 88.5773C289.477 70.0547 357.911 21.2951 363.196 2.67356C376.066 -42.6736 0 2.67356 0 2.67356V408Z" fill="url(#paint0_linear_0_226)" fill-rule="evenodd" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_226" x1="-320.118" x2="271.142" y1="245.206" y2="714.094">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="hidden! absolute top-0 right-0 desktop:block!" fill="none" height="718" viewBox="0 0 715 718" width="715" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M715 718C715 718 583.753 716.462 521.445 657.526C459.137 598.591 459.653 471.139 326.809 478.107C193.966 485.075 52.0071 456.569 102.409 376.286C152.811 296.002 270.536 305.954 298.673 175.094C326.809 44.2347 96.4371 202.259 58.3874 115.246C43.5286 81.2658 78.796 76.685 24.1143 38.4564C5.9611 25.7653 -2.8172 7.3759 0.802121 0.227893C2.54628 -3.21675 8.72623 1.80642 15.1713 0.227893C35.6384 -4.78487 78.9329 0.227893 122.813 0.227893C264.527 0.227893 715 0.227893 715 0.227893V718Z" fill="url(#paint0_linear_0_227)" fill-rule="evenodd" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_227" x1="555.135" x2="-306.366" y1="-433.769" y2="169.616">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="hidden! top-[612rem] left-[510rem] absolute desktop:block!" fill="none" height="44" viewBox="0 0 112 44" width="112" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M10.3957 0.229475C19.0964 -1.59006 21.8046 8.0084 37.6706 8.0084C53.5366 8.0084 67.6464 -1.05734 87.2817 6.9363C106.917 14.9299 122.804 39.1464 102.687 34.4771C90.8046 31.7189 76.1087 23.3102 55.8705 39.3188C35.6323 55.3274 23.5223 24.7586 8.35962 29.4505C-6.8031 34.1424 1.6248 2.0637 10.3957 0.229475Z" fill="url(#paint0_linear_0_439)" fill-rule="evenodd" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_439" x1="-66.1698" x2="120.224" y1="83.9239" y2="-41.7127">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="hidden! top-[134rem] left-[804rem] absolute desktop:block!" fill="none" height="63" viewBox="0 0 55 63" width="55" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M42.1046 6.75989C45.5429 4.48969 50.0974 7.11921 49.8505 11.232L47.3841 52.3237C47.1614 56.0339 43.112 58.2127 39.8931 56.3543L8.00629 37.9444C4.78736 36.086 4.64954 31.4897 7.75134 29.4417L42.1046 6.75989Z" fill-rule="evenodd" opacity="0.543387" stroke="url(#paint0_linear_0_225)" stroke-width="10" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_225" x1="-55.3523" x2="45.4596" y1="80.7294" y2="78.8246">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="hidden! top-[547rem] left-[948rem] absolute desktop:block!" fill="none" height="52" viewBox="0 0 52 52" width="52" xmlns="http://www.w3.org/2000/svg">
+		<rect fill="url(#paint0_linear_0_222)" height="38" rx="3" transform="rotate(45 25.8701 -1)" width="38" x="25.8701" y="-1"/>
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_222" x1="92.971" x2="59.9" y1="31.5241" y2="1.46447">
+				<stop stop-color="white" stop-opacity="0.01"/>
+				<stop offset="1" stop-color="#C9F0FF"/>
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="absolute top-0 left-[-40rem] z-1 desktop:hidden!" fill="none" height="321" viewBox="0 0 187 321" width="187" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M-40 321C-40 321 -18.3238 239.93 47.7767 259.383C113.877 278.837 113.877 278.128 159.096 245.305C172.659 235.46 164.22 188.9 143.887 187.374C123.554 185.848 139.081 152.313 174.09 145.733C209.099 139.154 164.69 92.6959 138.043 79.1941C110.369 65.1723 145.917 28.2608 148.663 14.1641C155.348 -20.1641 -40 14.1641 -40 14.1641V321Z" fill="url(#paint0_linear_0_5)" fill-rule="evenodd" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_5" x1="-206.286" x2="179.701" y1="197.763" y2="407.805">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<svg class="absolute top-0 right-0 z-1 desktop:hidden!" fill="none" height="535" viewBox="0 0 214 535" width="214" xmlns="http://www.w3.org/2000/svg">
+		<path clip-rule="evenodd" d="M214 535C214 535 174.718 534.086 156.069 499.052C137.42 464.018 137.574 388.255 97.8142 392.397C58.054 396.539 15.5658 379.594 30.651 331.87C45.7363 284.146 80.9717 290.061 89.393 212.273C97.8142 134.484 28.8637 228.421 17.4754 176.696C13.0281 156.497 23.5837 153.774 7.21743 131.049C1.78416 123.505 -0.84319 112.573 0.240075 108.324C0.240075 108.324 5.28841 0.938348 7.21743 3.09944e-06C13.3432 -2.9798 21.6328 -2 34.7662 -2C77.1812 -2 214 3.09944e-06 214 3.09944e-06V535Z" fill="url(#paint0_linear_0_6)" fill-rule="evenodd" />
+		<defs>
+			<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_6" x1="166.152" x2="-190.058" y1="-324.274" y2="-224.185">
+				<stop stop-color="white" stop-opacity="0.01" />
+				<stop offset="1" stop-color="#C9F0FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<div class="relative z-2 desktop:flex desktop:flex-col desktop:basis-[536rem] desktop:shrink-0 desktop:mt-[74rem] desktop:grow-1">
 		<h1 class="font-[var(--second-family)] mt-[unset]! font-semibold! text-[24rem]! leading-[150%]! text-[#00252e] text-center pt-[53rem]! mb-[24rem]! desktop:text-left desktop:text-[40rem]! desktop:leading-[150%]! desktop:mb-[32rem]! desktop:mt-[unset]! desktop:pt-[unset]!">Build The Community Your Fans Will Love</h1>
 		<p class="font-[var(--font-family)] font-normal! text-[16rem]! leading-[150%]! text-center text-[#00252e] mb-[32rem] desktop:text-left desktop:text-[18rem]! desktop:leading-[150%]! desktop:mr-[20rem]">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
 		<div class="text-center desktop:text-left">
 			<Link colorScheme="Pink" href="/">Get Started For Free</Link>
 		</div>
 	</div>
-	<div class="relative w-[279.69rem] h-[174.17rem] mt-[56rem] mr-[auto] ml-[auto] desktop:w-[642.52rem] desktop:h-[401.67rem] desktop:shrink-0 desktop:mt-[unset] desktop:mr-[unset] desktop:ml-[unset] desktop:mr-[35rem]!">
+	<div class="relative z-2 w-[279.69rem] h-[174.17rem] mt-[56rem] mr-[auto] ml-[auto] desktop:w-[642.52rem] desktop:h-[401.67rem] desktop:shrink-0 desktop:mt-[unset] desktop:mr-[unset] desktop:ml-[unset] desktop:mr-[35rem]!">
+		<svg class="absolute top-[-32rem] left-[180rem] z-1 desktop:hidden!" fill="none" height="18" viewBox="0 0 18 18" width="18" xmlns="http://www.w3.org/2000/svg">
+			<rect fill="url(#paint0_linear_0_218)" height="13" rx="1" transform="rotate(45 9.19238 -0.17157)" width="13" x="9.19238" y="-0.17157"/>
+			<defs>
+				<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_218" x1="-30.8961" x2="3.92338" y1="-11.9407" y2="26.4295">
+					<stop stop-color="white" stop-opacity="0.01"/>
+					<stop offset="1" stop-color="#DEF5FE"/>
+				</linearGradient>
+			</defs>
+		</svg>
+		<svg class="absolute top-[162rem] left-[-5rem] z-1 desktop:hidden!" fill="none" height="22" viewBox="0 0 22 22" width="22" xmlns="http://www.w3.org/2000/svg">
+			<rect fill="url(#paint0_linear_0_7)" height="16" rx="1.26316" transform="rotate(45 11 -0.313721)" width="16" x="11" y="-0.313721"/>
+			<defs>
+				<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_7" x1="39.253" x2="25.3284" y1="13.3807" y2="0.723951">
+					<stop stop-color="white" stop-opacity="0.01"/>
+					<stop offset="1" stop-color="#C9F0FF"/>
+				</linearGradient>
+			</defs>
+		</svg>
+		<svg class="absolute top-[-25rem] left-0 z-1 desktop:hidden!" fill="none" height="29" viewBox="0 0 25 29" width="25" xmlns="http://www.w3.org/2000/svg">
+			<path clip-rule="evenodd" d="M19.2213 3.10274C20.7783 2.07473 22.8407 3.26545 22.7289 5.12786L21.612 23.7354C21.5112 25.4155 19.6775 26.4021 18.2199 25.5606L3.78057 17.224C2.32294 16.3825 2.26053 14.3012 3.66512 13.3738L19.2213 3.10274Z" fill-rule="evenodd" opacity="0.543387" stroke="url(#paint0_linear_0_8)" stroke-width="4.5283" />
+			<defs>
+				<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_8" x1="-24.9101" x2="20.7405" y1="36.5984" y2="35.7358">
+					<stop stop-color="white" stop-opacity="0.01" />
+					<stop offset="1" stop-color="#C9F0FF" />
+				</linearGradient>
+			</defs>
+		</svg>
 		<svg class="w-[279.69rem] h-[174.17rem] relative z-1 desktop:w-[642.52rem] desktop:h-[401.67rem]" fill="none" height="416" viewBox="0 0 657 416" width="657" xmlns="http://www.w3.org/2000/svg">
 			<g filter="url(#filter0_d_0_329)">
 				<rect fill="white" height="401.667" rx="13.3858" width="642.52" x="7" y="7" />
