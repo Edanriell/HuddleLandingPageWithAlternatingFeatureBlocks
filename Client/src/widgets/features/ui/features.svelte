c6435b05a2bacf2d005e9a7cc8f4c17ab45fccc2
Edanriell <Edanriell@gmail.com>
2025-06-18 22:58:19 +0300

    feature(content-card-integration): added flex

--- Client/src/widgets/features/ui/features.svelte
+++ Client/src/widgets/features/ui/features.svelte
@@ -4,18 +4,18 @@
 
 <section>
 	<h2 class="visually-hidden">Key features</h2>
-	<ul> 
-		<li>
+	<ul class="flex flex-row gap-y-[40rem]">
+		<li class="relative">
 			<ContentCard description="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form." title="Grow Together">
 				<img alt="Grow Together" class="w-[236rem] h-[194rem] object-cover desktop:w-[406rem] desktop:h-[332rem]" src="/images/raster/grow_together.png">
 			</ContentCard>
 		</li>
-		<li>
+		<li class="relative">
 			<ContentCard description="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow." title="Flowing Conversations">
 				<img alt="Flowing Conversations" class="w-[242rem] h-[172rem] object-cover desktop:w-[418rem] desktop:h-[297rem]" src="/images/raster/flowing_conversations.png">
 			</ContentCard>
 		</li>
-		<li>
+		<li class="relative">
 			<ContentCard description="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately." title="Your Users">
 				<img alt="Your Users" class="w-[244rem] h-[185rem] object-cover desktop:w-[420rem] desktop:h-[318rem]" src="/images/raster/your_users.png">
 			</ContentCard>
