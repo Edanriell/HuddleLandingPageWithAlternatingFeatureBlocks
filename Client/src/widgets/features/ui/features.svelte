e5f32165cfef6e7775bf22f2974773f5f2cf0519
Edanriell <Edanriell@gmail.com>
2025-06-20 22:46:21 +0300

    feature(content-card-integration): finished working on card integration component

--- Client/src/widgets/features/ui/features.svelte
+++ Client/src/widgets/features/ui/features.svelte
@@ -2,21 +2,21 @@
 	import {ContentCard} from "@shared/ui/content-card/ui";
 </script>
 
-<section>
+<section class="ml-[20rem] mr-[20rem] pt-[59rem] pb-[120rem] desktop:pt-[160rem] desktop:ml-[104rem] desktop:mr-[104rem] desktop:pb-[160rem]">
 	<h2 class="visually-hidden">Key features</h2>
-	<ul class="flex flex-row gap-y-[40rem]">
+	<ul class="flex flex-col gap-y-[40rem]">
 		<li class="relative">
-			<ContentCard description="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form." title="Grow Together">
+			<ContentCard className="desktop:flex-row-reverse" contentClassName="desktop:basis-[480rem]" description="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form." title="Grow Together">
 				<img alt="Grow Together" class="w-[236rem] h-[194rem] object-cover desktop:w-[406rem] desktop:h-[332rem]" src="/images/raster/grow_together.png">
 			</ContentCard>
 		</li>
 		<li class="relative">
-			<ContentCard description="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow." title="Flowing Conversations">
+			<ContentCard contentClassName="desktop:basis-[498rem]" description="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow." title="Flowing Conversations">
 				<img alt="Flowing Conversations" class="w-[242rem] h-[172rem] object-cover desktop:w-[418rem] desktop:h-[297rem]" src="/images/raster/flowing_conversations.png">
 			</ContentCard>
 		</li>
 		<li class="relative">
-			<ContentCard description="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately." title="Your Users">
+			<ContentCard className="desktop:flex-row-reverse" contentClassName="desktop:basis-[480rem]" description="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately." title="Your Users">
 				<img alt="Your Users" class="w-[244rem] h-[185rem] object-cover desktop:w-[420rem] desktop:h-[318rem]" src="/images/raster/your_users.png">
 			</ContentCard>
 		</li>
