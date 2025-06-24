a7444123b01ad44fbe39a8a30065db7f74c2c58c
Edanriell <Edanriell@gmail.com>
2025-06-24 23:08:52 +0300

    feature(hover-animations): created hover animations

--- Client/src/shared/ui/secondary-navigation/ui/secondary-navigation.svelte
+++ Client/src/shared/ui/secondary-navigation/ui/secondary-navigation.svelte
@@ -1,22 +1,25 @@
+<script>
+</script>
+
 <nav aria-label="Secondary navigation" class="relative mt-[41rem] desktop:mt-[unset]">
 	<ul class="grid grid-rows-[auto] grid-cols-1 gap-y-[16rem] desktop:grid-flow-row desktop:grid-cols-2 desktop:gap-y-[12rem] desktop:gap-x-[143rem]">
 		<li class="relative desktop:order-1">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">About Us</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">About Us</a>
 		</li>
 		<li class="relative desktop:order-3">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">What We Do</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">What We Do</a>
 		</li>
 		<li class="relative desktop:order-5">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">FAQ</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">FAQ</a>
 		</li>
 		<li class="relative desktop:order-2">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Career</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">Career</a>
 		</li>
 		<li class="relative desktop:order-4">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Blog</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">Blog</a>
 		</li>
 		<li class="relative desktop:order-6">
-			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white" href="/">Contact Us</a>
+			<a class="font-[var(--font-family)] font-normal text-[14rem] leading-[193%] text-white hover:underline" href="/">Contact Us</a>
 		</li>
 	</ul>
 </nav>
