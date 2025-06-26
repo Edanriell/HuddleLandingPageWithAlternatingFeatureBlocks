0b0e72a35ac16386706db29cefc319f47b3b1de6
Edan <60575156+Edanriell@users.noreply.github.com>
2025-06-26 19:57:14 +0300

    Update logotype.svelte

--- Client/src/shared/ui/logotype/ui/logotype.svelte
+++ Client/src/shared/ui/logotype/ui/logotype.svelte
@@ -4,10 +4,10 @@
 
 {#if colorScheme === "Dark"}
 	<a href="/" class="relative">
-		<img class="w-[104rem] h-[16rem] desktop:w-[200rem] desktop:w-[31rem] object-contain" alt="Huddle company logotype" src="/images/raster/logotype_dark.png">
+		<img class="w-[104rem] h-[16rem] desktop:w-[200rem] desktop:h-[31rem] object-contain" alt="Huddle company logotype" src="/images/raster/logotype_dark.png">
 	</a>
 {:else}
 	<a href="/" class="relative">
-		<img class="w-[167rem] h-[26rem] desktop:w-[152rem] desktop:w-[24rem] object-contain" alt="Huddle company logotype" src="/images/raster/logotype_light.png">
+		<img class="w-[167rem] h-[26rem] desktop:w-[152rem] desktop:h-[24rem] object-contain" alt="Huddle company logotype" src="/images/raster/logotype_light.png">
 	</a>
 {/if}
